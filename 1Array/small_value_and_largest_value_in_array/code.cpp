9a50920fcd34f5ed3ee30dac1db8a88088e2c319
Hammad Ali <hammadali78056@gmail.com>
2025-06-22 15:30:43 +0500

    new updation

--- 1Array/small_value_and_largest_value_in_array/code.cpp
+++ 1Array/small_value_and_largest_value_in_array/code.cpp
@@ -4,12 +4,15 @@ using namespace std;
 int nums[]={5, 3, 16, 20, 0};
 int size = 5;
  int smallest = INT_MAX;
+ int largest = INT_MIN;
 
  for(int i=0; i<size; i++) 
  {
-   smallest= min(nums[i],smallest)
+   smallest= min(nums[i],smallest);
+   largest = max(muns[i], largest);
+   
  }
 cout<< "Smallest value is "<< smallest<< endl;
-
+cout<< "largest value is "<< largest <<endl;
     return 0;
  }

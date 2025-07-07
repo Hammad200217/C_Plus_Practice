608e18c04cea81f8af8154cd2be9ab744b21917d
Hammad Ali <hammadali78056@gmail.com>
2025-07-07 10:41:49 +0500

    vector syntax example

---
+++ 1Vectors/vectorSyntax/code.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+ using namespace std;
+
+ int main(){
+ vector<int> v1= {1, 2, 3, 4, 5};
+ 
+    cout <<v1[0] << endl; // Accessing the first element
+
+    return 0;
+ }
